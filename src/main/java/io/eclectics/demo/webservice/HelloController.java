2f9689911dfe891f9a74e69ad34f0521f2d8f4ca
chacha <john.chacha@eclectics.io>
2024-07-27 06:50:54 +0300

    ft - initial commit of sample spring boot project

---
+++ src/main/java/io/eclectics/demo/webservice/HelloController.java
@@ -0,0 +1,13 @@
+package io.eclectics.demo.webservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello DevOps!";
+	}
+}
